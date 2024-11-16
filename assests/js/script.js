4bf477bcbeb826329d63fe02d6fed9cc4abd440b
Nikhil Patel <68939870+naive-nikhil@users.noreply.github.com>
2024-11-16 14:41:28 +0530

    Added responsiveness for project's gallery section

--- assests/js/script.js
+++ assests/js/script.js
@@ -24,3 +24,39 @@ for (let btn of navBtns) {
     activeSection(btn);
   });
 }
+
+const projectsWrapper = document.querySelector(".projects-wrapper");
+const leftArrow = document.querySelector(".arrow-left");
+const rightArrow = document.querySelector(".arrow-right");
+
+let isScrolling = false;
+
+function scrollProjects(direction) {
+  if (isScrolling) return;
+  isScrolling = true;
+
+  const projectWidth = document.querySelector(".project").offsetWidth; // Get the width of one project
+  const projectHeight = document.querySelector(".project").offsetHeight;
+  const scrollAmountWidth = direction === "left" ? -projectWidth : projectWidth;
+  const scrollAmountHeight =
+    direction === "left" ? -projectHeight : projectHeight;
+
+  if (window.innerWidth > 1138) {
+    projectsWrapper.scrollBy({
+      left: scrollAmountWidth,
+      behavior: "smooth",
+    });
+  } else {
+    projectsWrapper.scrollBy({
+      top: scrollAmountHeight,
+      behavior: "smooth",
+    });
+  }
+
+  setTimeout(() => {
+    isScrolling = false;
+  }, 500);
+}
+
+leftArrow.addEventListener("click", () => scrollProjects("left"));
+rightArrow.addEventListener("click", () => scrollProjects("right"));
